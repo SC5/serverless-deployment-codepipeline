9fed76ff616989169d4127a4a0a8d3b1db346461
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-03-11 09:12:43 +0200

    add deployment script and CodeCommit repository, Codebuild job and service role to cloudformation template

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+STACK_NAME=serverless-example-pipeline-deployment
+PIPELINE_NAME=serverless-example-pipeline
+CODEBUILD_NAME=serverless-example-codebuild
+REPOSITORY_NAME=serverless-example-repository
+
+aws cloudformation deploy \
+  --template-file pipeline-stack.yml \
+  --stack-name $STACK_NAME \
+  --capabilities CAPABILITY_IAM \
+  --parameter-overrides \
+  RepositoryName=$REPOSITORY_NAME \
+  PipelineName=$PIPELINE_NAME \
+  CodeBuildName=$CODEBUILD_NAME
